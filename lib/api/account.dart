300a5067ab02ebfd7df21c4e90b9c4644198cc84
12vv <yegarrick12@gmail.com>
2023-12-01 10:37:11 +0800

    fix: login dialog

--- lib/api/account.dart
+++ lib/api/account.dart
@@ -4,14 +4,18 @@ import "package:restaurant_counter/api/utils.dart";
 import '../models/session.dart';
 import "config.dart";
 
-Future<Session> signinApi(String email, String password) async {
+Future<dynamic> signinApi(String email, String password) async {
   var body = jsonEncode({'email': email, 'password': password});
-  final response = await http.post(Uri.parse("$baseUrl/sessions"),
-      body: body, headers: {'Authorization': await getToken()});
+  try {
+    final response = await http.post(Uri.parse("$baseUrl/sessions"),
+        body: body, headers: {'Authorization': await getToken()});
 
-  if (response.statusCode == 201) {
-    return Session.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failed to Signin');
+    if (response.statusCode == 201) {
+      return Session.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception(response.statusCode);
+    }
+  } catch (e) {
+    return Future.error(e);
   }
 }
